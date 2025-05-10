e991fa501fac4a9cf1d116b8a9384763230affb5
Krual <tsk2455014103@outlook.com>
2025-05-10 20:02:27 +0800

    feat: 添加补件检索训练配置类

---
+++ src/trains/configs/complementary_item_retrieval_train_config.py
@@ -0,0 +1,18 @@
+from typing import Literal
+
+from src.trains.configs import BaseTrainConfig
+
+
+class ComplementaryItemRetrievalTrainConfig(BaseTrainConfig):
+    find_unused_parameters = True
+    batch_size: int = 4096
+    dataloader_workers: int = 4
+    polyvore_type: Literal['nondisjoint', 'disjoint'] = 'nondisjoint'
+    name: str = 'complementary_item_retrieval'
+    auto_save_checkpoint: bool = True
+    load_image: bool = False
+    learning_rate: float = 2e-5 # learning rate
+    n_epochs: int = 200
+    accumulation_steps: int = 4
+    margin: float = 2.0
+
