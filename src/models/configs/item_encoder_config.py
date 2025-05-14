57545341672d53e7b012e1fe5c42701a91954544
Krual <tsk2455014103@outlook.com>
2025-05-14 17:51:41 +0800

    feat(item_encoder_config): 添加CLIP模型配置及优化

--- src/models/configs/item_encoder_config.py
+++ src/models/configs/item_encoder_config.py
@@ -4,7 +4,8 @@ from typing import Literal
 
 @dataclass
 class ItemEncoderConfig:
-
+    # Contrastive Language-Image Pre-Training (CLIP) 对比语言-图像预训练模型
+    #  Sigmoid loss for Language-Image Pre-training (SigLIP)
     type: Literal['clip', 'resnet_hf_sentence_bert', 'slip'] = 'slip'
 
     norm_out: bool = True
@@ -13,8 +14,9 @@ class ItemEncoderConfig:
 
     def __post_init__(self):
         # 根据不同的类型配置不同的encoder
+        dim_per_modality_embed = 0
         if self.type == 'clip':
-            self.clip_model_name: str = "patrickjohncyh/fashion-clip"#"Marqo/marqo-fashionCLIP"
+            self.clip_model_name: str = "patrickjohncyh/fashion-clip"# "Marqo/marqo-fashionCLIP"不支持HF的CLIP API 但支持open_clip
             dim_per_modality_embed = 512
         elif self.type == 'resnet_hf_sentence_bert':
             self.text_model_name: str = "sentence-transformers/all-MiniLM-L6-v2"
