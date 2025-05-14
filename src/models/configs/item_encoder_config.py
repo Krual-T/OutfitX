7490be56a159bdeb4b80686f3b91ace8f5ecdd72
Krual <tsk2455014103@outlook.com>
2025-05-14 09:36:07 +0800

    feat(item_encoder): 添加slip类型支持并更新模型配置

--- src/models/configs/item_encoder_config.py
+++ src/models/configs/item_encoder_config.py
@@ -5,7 +5,7 @@ from typing import Literal
 @dataclass
 class ItemEncoderConfig:
 
-    type: Literal['clip', 'resnet_hf_sentence_bert'] = 'clip'
+    type: Literal['clip', 'resnet_hf_sentence_bert', 'slip'] = 'slip'
 
     norm_out: bool = True
     # 定义聚合方法，可选值为 'concat' 、 'sum' 或 'mean'，默认为 'concat'
@@ -14,9 +14,12 @@ class ItemEncoderConfig:
     def __post_init__(self):
         # 根据不同的类型配置不同的encoder
         if self.type == 'clip':
-            self.clip_model_name: str = "Marqo/marqo-fashionCLIP"# "Marqo/marqo-fashionSigLIP"#"patrickjohncyh/fashion-clip"
+            self.clip_model_name: str = "patrickjohncyh/fashion-clip"#"Marqo/marqo-fashionCLIP"
             dim_per_modality_embed = 512
         elif self.type == 'resnet_hf_sentence_bert':
             self.text_model_name: str = "sentence-transformers/all-MiniLM-L6-v2"
             dim_per_modality_embed = 64
+        elif self.type =='slip':
+            self.slip_model_name: str = "Marqo/marqo-fashionSigLIP"
+            dim_per_modality_embed = 768
         self.dim_per_modality:int = dim_per_modality_embed
