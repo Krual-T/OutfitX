3e09f9609bdeedfcb52a695c8d8455aa67f3e781
Krual <tsk2455014103@outlook.com>
2025-05-14 21:49:17 +0800

    refactor(precompute_embedding): 调整批量大小为2048

--- src/trains/configs/precompute_embedding_config.py
+++ src/trains/configs/precompute_embedding_config.py
@@ -7,7 +7,7 @@ from .base_train_config import BaseTrainConfig
 @dataclass
 class PrecomputeEmbeddingConfig(BaseTrainConfig):
     find_unused_parameters = True
-    batch_size: int = 4096
+    batch_size: int = 2048
     dataloader_workers: int = 2
     run_name: str = 'precompute_embedding'
     n_epochs: int = 1
