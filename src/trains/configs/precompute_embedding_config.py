06edad5072a4e1ab4134418d9d5e9b5349a0fb31
Krual <tsk2455014103@outlook.com>
2025-05-14 21:47:46 +0800

    refactor(precompute_embedding): reduce dataloader_workers to 2

--- src/trains/configs/precompute_embedding_config.py
+++ src/trains/configs/precompute_embedding_config.py
@@ -8,7 +8,7 @@ from .base_train_config import BaseTrainConfig
 class PrecomputeEmbeddingConfig(BaseTrainConfig):
     find_unused_parameters = True
     batch_size: int = 4096
-    dataloader_workers: int = 4
+    dataloader_workers: int = 2
     run_name: str = 'precompute_embedding'
     n_epochs: int = 1
     auto_save_checkpoint: bool = False
