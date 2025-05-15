a5039d6c7ae4bca979212342d627e9bedd2098a1
Krual <tsk2455014103@outlook.com>
2025-05-15 13:57:54 +0800

    refactor(train-configs): 调整批量大小和工作线程数

--- src/trains/configs/complementary_item_retrieval_train_config.py
+++ src/trains/configs/complementary_item_retrieval_train_config.py
@@ -5,8 +5,8 @@ from src.trains.configs import BaseTrainConfig
 
 class ComplementaryItemRetrievalTrainConfig(BaseTrainConfig):
     find_unused_parameters = True
-    batch_size: int = 4096
-    dataloader_workers: int = 8
+    batch_size: int = 2048
+    dataloader_workers: int = 4
     polyvore_type: Literal['nondisjoint', 'disjoint'] = 'nondisjoint'
     run_name: str = 'complementary_item_retrieval'
     auto_save_checkpoint: bool = True
