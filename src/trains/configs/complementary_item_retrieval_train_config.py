c6b45cdbd76c7832c423fe3ac863606db0386f67
Krual <tsk2455014103@outlook.com>
2025-05-16 11:10:56 +0800

    fix: 调整互补项目检索训练的batch_size为1024*3

--- src/trains/configs/complementary_item_retrieval_train_config.py
+++ src/trains/configs/complementary_item_retrieval_train_config.py
@@ -5,7 +5,7 @@ from src.trains.configs import BaseTrainConfig
 
 class ComplementaryItemRetrievalTrainConfig(BaseTrainConfig):
     find_unused_parameters = True
-    batch_size: int = 1024*4
+    batch_size: int = 1024*3
     dataloader_workers: int = 8
     polyvore_type: Literal['nondisjoint', 'disjoint'] = 'nondisjoint'
     run_name: str = 'complementary_item_retrieval'
