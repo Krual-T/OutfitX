3e028a76ec62a8de93e3670d0512f24811125bf4
Krual <tsk2455014103@outlook.com>
2025-05-19 13:11:40 +0800

    refactor(complementary_item_retrieval): 调整训练周期及硬切换周期

--- src/trains/configs/complementary_item_retrieval_train_config.py
+++ src/trains/configs/complementary_item_retrieval_train_config.py
@@ -12,8 +12,8 @@ class ComplementaryItemRetrievalTrainConfig(BaseTrainConfig):
     auto_save_checkpoint: bool = True
     load_image: bool = False
     learning_rate: float = 4e-5 # learning rate
-    n_epochs: int = 400
-    switch_to_hard_n_epochs: int = 200
+    n_epochs: int = 300
+    switch_to_hard_n_epochs: int = 150
     accumulation_steps: int = 4
     margin: float = 2.0
 
