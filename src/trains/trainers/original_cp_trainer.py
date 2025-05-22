5cd3721c74542a20c4a6c5a3d6e3154a4c6606f2
Krual <tsk2455014103@outlook.com>
2025-05-22 16:19:00 +0800

    refactor(original_cp_trainer): 调整dataloader_workers参数

--- src/trains/trainers/original_cp_trainer.py
+++ src/trains/trainers/original_cp_trainer.py
@@ -29,8 +29,7 @@ class OriginalCompatibilityPredictionTrainer(DistributedTrainer):
                 batch_size=100,
                 broadcast_buffers=False,
                 accumulation_steps= 1,
-                dataloader_workers=8,
-                use_amp=True,
+                dataloader_workers=16
             )
         super().__init__(cfg=cfg, run_mode=run_mode)
         self.cfg = cast(CompatibilityPredictionTrainConfig, cfg)
