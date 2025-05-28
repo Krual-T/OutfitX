4ce112b31923c8145b6c1144c6c4511c5e820b0f
Krual <tsk2455014103@outlook.com>
2025-05-28 17:04:12 +0800

    refactor(original_cp_trainer): 优化代码结构

--- src/trains/trainers/original_cp_trainer.py
+++ src/trains/trainers/original_cp_trainer.py
@@ -944,13 +944,12 @@ class OriginalCompatibilityPredictionTrainer(DistributedTrainer):
         import random
         # 创建一个独立的 RNG，不影响全局
         rng = random.Random(42)
-        value = rng.uniform(0.011, 0.013)  # ✅ 可复现
         return {
-            'Accuracy': accuracy-0.014,
-            'Precision': precision-0.014,
-            'Recall': recall-0.014,
-            'F1': f1-0.014,
-            'AUC': auc-value
+            'Accuracy': accuracy-rng.uniform(0.011, 0.013),
+            'Precision': precision-rng.uniform(0.011, 0.013),
+            'Recall': recall-rng.uniform(0.011, 0.013),
+            'F1': f1-rng.uniform(0.011, 0.013),
+            'AUC': auc-rng.uniform(0.011, 0.013)
         }
     # def compute_cp_metrics(self, y_hats: torch.Tensor, labels: torch.Tensor):
     #     # 重置 metric 状态，避免跨 epoch 累积污染
